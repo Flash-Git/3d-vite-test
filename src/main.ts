90efcdcb0dbef18fc7c2e3c84116cfbbb427ea5c
Quinn <git@jaquinn.com>
2022-02-21 19:21:08 +0000

    Organised code

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,21 @@
-import "./style.css";
-
-// const app = document.querySelector<HTMLDivElement>("#app")!;
-
-// app.innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `;
-
 import * as THREE from "three";
-
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import "./style.css";
 
 // Setup
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#bg") ?? undefined });
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
 renderer.setPixelRatio(window.devicePixelRatio);
 // Full Screen
 renderer.setSize(window.innerWidth, window.innerHeight);
-// Move Camera back
-// camera.position.setZ(30);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Camera Setup
+// You cannot set controls.target and camera.position to the same value.
+// camera.position.z = -30; //  .position.setZ(30);
+controls.target.set(0, 0, -1);
 
 // Lighting
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
@@ -32,37 +23,10 @@ const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(20, 5, 5);
 scene.add(pointLight, ambientLight);
 
+// HelperGrids
 const pointLightHelper = new THREE.PointLightHelper(pointLight);
 const gripHelper = new THREE.GridHelper(200, 50);
 scene.add(pointLightHelper, gripHelper);
-
-// Objects
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    emissive: 0xffffff,
-    emissiveIntensity: 0.1,
-  });
-
-  const star = new THREE.Mesh(geometry, material);
-
-  const [x, y, z] = Array(3)
-    .fill(0)
-    .map(() => THREE.MathUtils.randFloatSpread(100));
-
-  star.position.set(x, y, z);
-
-  scene.add(star);
-}
-
-Array(200).fill(0).map(addStar);
 
 const spaceTexture = new THREE.TextureLoader().load("space.jpg");
 scene.background = spaceTexture;
@@ -70,23 +34,45 @@ scene.background = spaceTexture;
 const moonTexture = new THREE.TextureLoader().load("moon.jpg");
 const moonNormals = new THREE.TextureLoader().load("normal.jpg");
 
+// Objects
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: moonNormals })
 );
 moon.position.z = -30;
-// moon.position.setX(-10);
-
 scene.add(moon);
+
+const generateStars = (count: number, range: number) => {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    emissive: 0xffffff,
+    emissiveIntensity: 0.1,
+  });
+
+  for (let i = 0; i < count; i++) {
+    const star = new THREE.Mesh(geometry, material);
+    star.position.set(
+      THREE.MathUtils.randFloatSpread(range),
+      THREE.MathUtils.randFloatSpread(range),
+      THREE.MathUtils.randFloatSpread(range)
+    );
+    scene.add(star);
+  }
+};
+
+generateStars(1000, 500);
 
 function tick() {
   requestAnimationFrame(tick);
 
-  // torus.rotation.x += 0.01;
-  // torus.rotation.y += 0.005;
-  // torus.rotation.z += 0.0005;
-
-  // controls.update();
+  // z index is important
+  controls.update();
   renderer.render(scene, camera);
 }
 
@@ -97,8 +83,6 @@ function moveCamera() {
   moon.rotation.x += 0.05;
   moon.rotation.x += 0.075;
   moon.rotation.z += 0.05;
-  // console.log(t);
-  console.log(t, t * -0.01);
   camera.position.z = t * -0.01;
   camera.position.setX(t * -0.0002);
   camera.position.setY(t * -0.0002);
